7856ca433421b778644d66c0cf4a6006c08ef5b2
Barış Hanım İnce <81899033+barishanimince@users.noreply.github.com>
2022-11-09 19:26:45 +0300

    Add files via upload

---
+++ sayıbulma/Sayıbulma.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sayıbulma;
+
+/**
+ *
+ * @author BARIŞ HANIM İNCE
+ */
+public class Sayıbulma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+  
+        int deger=50;
+    }
+    
+}
